c84ad09525213728236d99a122d68f8cc8a6067d
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-07-05 18:03:12 +0200

    Update for login/registration logic

--- app/src/main/java/com/example/voglioisoldi/data/database/dao/UserDao.kt
+++ app/src/main/java/com/example/voglioisoldi/data/database/dao/UserDao.kt
@@ -10,6 +10,14 @@ interface UserDao {
     @Insert
     suspend fun insertUser(user: User)
 
+    //L'implementation di UserDao, da errore perchè non utilizziamo ancora tale funziona
     @Query("SELECT * FROM user")
     suspend fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM user WHERE username = :username AND passwordHash = :passwordHash")
+    suspend fun getUser(username: String, passwordHash: String): User?
+
+    //Ci dice se l'utente già esiste in fase di registrazione
+    @Query("SELECT COUNT(*) FROM user WHERE username = :username OR email = :email")
+    suspend fun existsUser(username: String, email: String): Int
 }
--- app/src/main/java/com/example/voglioisoldi/data/repositories/UserRepository.kt
+++ app/src/main/java/com/example/voglioisoldi/data/repositories/UserRepository.kt
@@ -1,9 +1,28 @@
 package com.example.voglioisoldi.data.repositories
 
 import com.example.voglioisoldi.data.database.dao.UserDao
+import com.example.voglioisoldi.data.database.entities.User
+import java.security.MessageDigest
 
 class UserRepository(
     private val dao: UserDao
 ) {
+    suspend fun insertUser(user: User) {
+        dao.insertUser(user)
+    }
 
+    suspend fun userExists(username: String, email: String): Boolean {
+        return dao.existsUser(username, email) > 0
+    }
+
+    suspend fun getUser(username: String, password: String): User? {
+        val passwordHash = hashPassword(password)
+        return dao.getUser(username, passwordHash)
+    }
+}
+//TODO: Da spostare in una util??
+fun hashPassword(password: String): String {
+    return MessageDigest.getInstance("SHA-256")
+        .digest(password.toByteArray())
+        .joinToString("") { "%02x".format(it) }
 }

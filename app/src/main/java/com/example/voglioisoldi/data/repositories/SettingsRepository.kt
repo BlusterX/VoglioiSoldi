b496266cfcc5dd9ced72c9cb501a27f08ea9f5fe
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-07-09 19:18:49 +0200

    Add app theme mode to settings repo

--- app/src/main/java/com/example/voglioisoldi/data/repositories/SettingsRepository.kt
+++ app/src/main/java/com/example/voglioisoldi/data/repositories/SettingsRepository.kt
@@ -2,12 +2,30 @@ package com.example.voglioisoldi.data.repositories
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.example.voglioisoldi.data.models.ThemeMode
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class SettingsRepository(
     private val dataStore: DataStore<Preferences>
 ) {
     companion object {
-
+        private val THEME_MODE_KEY = stringPreferencesKey("theme_mode")
     }
 
+    fun getThemeMode(): Flow<ThemeMode> {
+        return dataStore.data
+            .map { preferences ->
+                try {
+                    ThemeMode.valueOf(preferences[THEME_MODE_KEY] ?: ThemeMode.SYSTEM.name)
+                } catch (_: Exception) {
+                    ThemeMode.SYSTEM
+                }
+            }
+    }
+
+    suspend fun setThemeMode(themeMode: ThemeMode) =
+        dataStore.edit { it[THEME_MODE_KEY] = themeMode.name }
 }

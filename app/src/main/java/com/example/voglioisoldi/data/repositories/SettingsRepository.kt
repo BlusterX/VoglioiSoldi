cbfd91b0eedb746fe117eaf1f813aa1aa315551a
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-07-10 12:06:17 +0200

    Store username linked to biometric auth

--- app/src/main/java/com/example/voglioisoldi/data/repositories/SettingsRepository.kt
+++ app/src/main/java/com/example/voglioisoldi/data/repositories/SettingsRepository.kt
@@ -13,6 +13,7 @@ class SettingsRepository(
 ) {
     companion object {
         private val THEME_MODE_KEY = stringPreferencesKey("theme_mode")
+        private val BIOMETRIC_USERNAME_KEY = stringPreferencesKey("biometric_username")
     }
 
     fun getThemeMode(): Flow<ThemeMode> {
@@ -28,4 +29,19 @@ class SettingsRepository(
 
     suspend fun setThemeMode(themeMode: ThemeMode) =
         dataStore.edit { it[THEME_MODE_KEY] = themeMode.name }
+
+    fun getBiometricUsername(): Flow<String?> {
+        return dataStore.data.map { preferences ->
+            preferences[BIOMETRIC_USERNAME_KEY]
+        }
+    }
+
+    suspend fun setBiometricUsername(username: String?) =
+        dataStore.edit { preferences ->
+            if (username != null) {
+                preferences[BIOMETRIC_USERNAME_KEY] = username
+            } else {
+                preferences.remove(BIOMETRIC_USERNAME_KEY)
+            }
+        }
 }

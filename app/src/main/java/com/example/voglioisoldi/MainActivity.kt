7bd857c57bd2c91cac190b30b4a13e7e0cdcfbba
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-07-11 16:08:04 +0200

    Light fix

--- app/src/main/java/com/example/voglioisoldi/MainActivity.kt
+++ app/src/main/java/com/example/voglioisoldi/MainActivity.kt
@@ -10,35 +10,25 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.voglioisoldi.data.models.ThemeMode
 import com.example.voglioisoldi.data.repositories.SettingsRepository
 import com.example.voglioisoldi.ui.Navigation
 import com.example.voglioisoldi.ui.SoldiRoute
 import com.example.voglioisoldi.ui.theme.VoglioiSoldiTheme
+import com.example.voglioisoldi.workers.RecurringTransactionWorker
 import org.koin.android.ext.android.get
 
 //Al posto di Text("Screen 2") -> Text(stringResource(R.string.screen2_name))
 class MainActivity : FragmentActivity() {
-    private val requestNotificationPermission = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (!isGranted) {
-            //Nel caso non accettassero la richiesta per le notifiche...
-            Toast.makeText(
-                this,
-                "Senza permesso non riceverai notifiche automatiche.",
-                Toast.LENGTH_LONG
-            ).show()        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val settingsRepository: SettingsRepository = get()
 
-        // Chiedi qui il permesso
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requestNotificationPermission.launch(android.Manifest.permission.POST_NOTIFICATIONS)
         }
-
         setContent {
             val themeMode by settingsRepository.getThemeMode().collectAsState(initial = ThemeMode.SYSTEM)
             VoglioiSoldiTheme(
@@ -56,9 +46,19 @@ class MainActivity : FragmentActivity() {
             }
         }
         //UTILE: Per far partire il worker subito, per testare la ricorrenza delle transazioni
-//        WorkManager.getInstance(this).enqueue(
-//            OneTimeWorkRequestBuilder<RecurringTransactionWorker>().build()
-//        )
+        WorkManager.getInstance(this).enqueue(
+            OneTimeWorkRequestBuilder<RecurringTransactionWorker>().build()
+        )
+    }
+    private val requestNotificationPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (!isGranted) {
+            Toast.makeText(
+                this,
+                "Senza permesso non riceverai notifiche automatiche.",
+                Toast.LENGTH_LONG
+            ).show()        }
     }
 }
 
